0712b6c69ca1dbbbcbe026c8962b77dc41b7ff8d
Aydin Elhan <aydinmate@protonmail.com>
2023-08-31 16:43:30 +1000

    better volume finder

--- main.py
+++ main.py
@@ -8,39 +8,39 @@ import asyncio
 
 scraper = Scrape
 
-async def scrape_data(date):  # date argument added
-    if date or not date: 
-        print(f"Scraping data for date: {date}")
+# async def scrape_data(date):  # date argument added
+#     if date or not date: 
+#         print(f"Scraping data for date: {date}")
 
-    url = os.getenv("PROVANS_WEBSITE")
-    scraper = Scrape(url, date)
-    return await scraper.scrape_orders()
+#     url = os.getenv("PROVANS_WEBSITE")
+#     scraper = Scrape(url, date)
+#     return await scraper.scrape_orders()
 
-loop = asyncio.get_event_loop()
-data = loop.run_until_complete(scrape_data("2023-08-31"))
+# loop = asyncio.get_event_loop()
+# data = loop.run_until_complete(scrape_data("2023-08-31"))
 
-# load_dotenv()
+load_dotenv()
 
-# google_maps_api_key = os.getenv("GOOGLE_MAPS_API")
+google_maps_api_key = os.getenv("GOOGLE_MAPS_API")
 
-# def read_database_file():
-#     with open('all_orders.json', 'r') as f:
-#         return json.load(f)
+def read_database_file():
+    with open('all_orders2.json', 'r') as f:
+        return json.load(f)
 
-# api_key = google_maps_api_key
-# database_jobs = read_database_file()
-
-# volume_finder = VolumeFinder(database_jobs)
-# formatted_details = volume_finder.get_updated_jobs()
-# volume_finder.save_to_file()
-
-# print(formatted_details)
-
+api_key = google_maps_api_key
+database_jobs = read_database_file()
 
 # job_formatter = AddressFinder(api_key)
 # database_jobs = job_formatter.format_job_details(database_jobs)
 
-# print(database_jobs)
+volume_finder = VolumeFinder(database_jobs)
+formatted_details = volume_finder.get_updated_jobs()
+volume_finder.save_to_file()
+
+# print(formatted_details)
+
+
+
 
 
 # update address_finder so it is like volume finder. add total volume to order.
--- volume_finder.py
+++ volume_finder.py
@@ -8,51 +8,100 @@ class VolumeFinder:
         else:
             self.jobs = jobs
         
+        filtered_jobs = []
+
         for job in self.jobs:
             order_items = job['order_items']
 
-            for item in order_items:
-                # If UOM: "LM"
-                if item["UOM"] == "LM":
-                    description = item["Description"]
-                    numbers = [int(num) for num in re.findall(r'\d+', description)]
-                    numbers.sort(reverse=True)
+            # Check if any SKU contains "DELSOS"
+            if any("DELSOS" in item.get("SKU", "") for item in order_items):
+                print(f"Skipping job {job['order_number']} due to SKU containing 'DELSOS'")
+            else:
+                filtered_jobs.append(job)
 
-                    if len(numbers) > 2:
-                        numbers = numbers[:2]
+        # Replace the original jobs list with the filtered one
+        self.jobs = filtered_jobs
+
+            
+
+        for item in order_items:
+            
+            sku = item["SKU"].strip().upper()
+            uom = item["UOM"].strip().upper()
+            description = item["Description"].lower()
+
+            if uom == "LM":
+                
+                numbers = [int(num) for num in re.findall(r'\d+', description)]
+                numbers.sort(reverse=True)
+
+                if len(numbers) > 2:
+                    numbers = numbers[:2]
+                
+                width = self.mm_to_m(numbers[0])
+                height = self.mm_to_m(numbers[1])
+                lineal_meters = float(item["Qty Ordered"])  # Convert to float
+                volume = width * height * lineal_meters
+                
+                item["width"] = width
+                item["height"] = height
+                item["lineal_meters"] = lineal_meters
+                item["volume"] = volume
+
+            elif uom == "EA" and not (sku[0].isdigit() and len(sku) > 6) and not sku.startswith(("DEL", "HIT", "WTAPE", "SB", "AB", "SSZP", "HB", "MN", "FS", "THOR", "EPA", "ANAS", "SEL")):
+                numbers = [int(num) for num in re.findall(r'\d+', description) if int(num) <= 10000]
+
+                if len(numbers) == 6:
+                    numbers = [numbers[3], numbers[2], numbers[3]]
                     
-                    width = self.mm_to_m(numbers[0])
-                    height = self.mm_to_m(numbers[1])
-                    lineal_meters = float(item["Qty Ordered"])  # Convert to float
-                    volume = width * height * lineal_meters
+                elif len(numbers) == 5 and "jamb" in description:
+                    numbers = [next(num for num in numbers if 100 < num < 200), max(numbers), next(num for num in numbers if 10 < num < 25)*3]
+
+                elif len(numbers) == 5 and not "jamb" in description:
+                    print(f"This product: {description}, has 5 numbers but is unfiltered. The volume is set to 0 - {numbers}")
+                    numbers = [0, 0, 0]
+                
+                elif len(numbers) == 4 and item["Special Order"] == "Y":
+                    numbers = [next(num for num in numbers if 300 < num < 1250), max(numbers), next(num for num in numbers if 30 < num < 60)]
                     
-                    item["width"] = width
-                    item["height"] = height
-                    item["lineal_meters"] = lineal_meters
-                    item["volume"] = volume
+                    
+            
+                elif len(numbers) == 4 and not item["Special Order"] == "Y":
+                    if "packer" in description or "hardiflex" in description:
+                        numbers = [numbers[1], numbers[0], float(str(numbers[2]) + "." + str(numbers[3]))]
+                        
+                        
+                    elif "sleeper" in description:
+                        numbers = [numbers[0], int(float(str(numbers[2]) + "." + str(numbers[3])) * 1000), numbers[1]]
 
-                elif item["UOM"] == "EA" and not item["SKU"][0].isdigit() and item["SKU"] != ("DEL" or "HIT" or "WTAPE"):
-                    description = item["Description"]
-                    numbers = [int(num) for num in re.findall(r'\d+', description)]
+                    elif ("flooring" and "tongue") in description:
+                        numbers = [numbers[2], int(float(str(numbers[0]) + "." + str(numbers[1])) * 1000), numbers[3]]
+                        
+                    elif ("plywood" and "flooring") in description:
+                        numbers = [numbers[2], numbers[1], numbers [3]]
 
-                    if len(numbers) > 3:
-                        concatenated_number = int(str(numbers[0]) + str(numbers[1]))*100
-                        numbers = [concatenated_number] + numbers[2:]
+                    elif ("axon") in description:
+                        numbers = [numbers[1], numbers[0], numbers[2]]
+                    else:
+                        print(f"This product: {description}, has 4 numbers but is unfiltered. The volume is set to 0 - {numbers}")
+                        numbers = [0, 0, 0]
+
+                
                     
 
-                    width = self.mm_to_m(numbers[0])
-                    length = self.mm_to_m(numbers[1])
-                    height = self.mm_to_m(numbers[2])
-                    quantity = float(item["Qty Ordered"])
-                    volume = width * length * height * quantity
+                width = self.mm_to_m(numbers[0])
+                length = self.mm_to_m(numbers[1])
+                height = self.mm_to_m(numbers[2])
+                quantity = float(item["Qty Ordered"])
+                volume = width * length * height * quantity
 
-                    item["width"] = width
-                    item["length"] = length
-                    item["height"] = height
-                    item["volume"] = volume
+                item["width"] = width
+                item["length"] = length
+                item["height"] = height
+                item["volume"] = volume
 
-                else:
-                    item["volume"] = 0
+            else:
+                item["volume"] = 0
 
 
     def get_updated_jobs(self):
